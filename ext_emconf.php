2550f1f7fd2fd31ddd3b5a254e423bfad3404c75
r3h6 <r3h6@outlook.com>
2016-01-23 21:32:19 +0100

    [TASK] Set version to 1.0.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -25,7 +25,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '7.6.0-7.6.99',
-			'jobqueue' => '0.0.1',
+			'jobqueue' => '1.0.0-1.0.99',
 		),
 		'conflicts' => array(
 		),
