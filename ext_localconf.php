9e1198fc21b7f628804d2422828e13a63c8eac5c
r3h6 <r3h6@outlook.com>
2016-04-29 23:14:02 +0200

    [TASk] Localconf

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,12 +3,11 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-
-if (isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY])) {
-    $GLOBALS['TYPO3_CONF_VARS']['EXT']['jobqueue']['TYPO3\\JobqueueBeanstalkd\\Queue\\BeanstalkdQueue'] = (array) unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
-} else {
-    $GLOBALS['TYPO3_CONF_VARS']['EXT']['jobqueue']['TYPO3\\JobqueueBeanstalkd\\Queue\\BeanstalkdQueue'] = [];
+if (!is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['jobqueue'])) {
+    $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['jobqueue'] = [];
 }
+
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['jobqueue']['TYPO3\\JobqueueBeanstalkd\\Queue\\BeanstalkdQueue'] = (array) unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
 
 if (!class_exists('Pheanstalk\\Pheanstalk', true)) {
     require_once \TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName('EXT:' . $_EXTKEY . '/Resources/Private/Vendors/pheanstalk.phar');
