d2b72d3edbe56d64bca4262a873950bd5cba39d7
r3h6 <r3h6@outlook.com>
2016-05-14 20:17:37 +0200

    [BUGFIX] Default options

--- ext_localconf.php
+++ ext_localconf.php
@@ -7,7 +7,9 @@ if (!is_array($GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['jobqueue'])) {
     $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['jobqueue'] = [];
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['jobqueue']['TYPO3\\JobqueueBeanstalkd\\Queue\\BeanstalkdQueue'] = (array) unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]);
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['jobqueue']['TYPO3\\JobqueueBeanstalkd\\Queue\\BeanstalkdQueue'] = [
+    'options' => (array) unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf'][$_EXTKEY]),
+];
 
 if (!class_exists('Pheanstalk\\Pheanstalk', true)) {
     require_once \TYPO3\CMS\Core\Utility\GeneralUtility::getFileAbsFileName('EXT:' . $_EXTKEY . '/Resources/Private/Vendors/pheanstalk.phar');
